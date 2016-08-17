813ff78ee50abcd42c970f32ff910ba781b98ec6
Robert Havelka <robert.havelka@artin.cz>
2016-08-17 13:56:26 +0200

    running for president works now 100%

--- src/runningForPresident/Main.java
+++ src/runningForPresident/Main.java
@@ -1,3 +1,75 @@
+/*
+Running for president
+        Challenge Description:
+
+        You're planning your party's campaign strategy to win the Presidency of the United States.
+        To win, your candidate has to navigate many controversial issues, which influence voters, such as healthcare, immigration, education, energy, independence, jobs, taxes, environment, and so on. Each of these is important, but also expensive to address.
+
+        Because the United States presidential election is determined by states casting votes in the Electoral College, you need minimum 270 of the total 538 Electoral College votes to win.
+
+        While not completely accurate, for this challenge we use the following criteria:
+        •States may or may not value an issue.
+        •States value each issue differently.
+        • Winning 51% of any state gives you all of that state's votes. If you have less than 51 %, you won't receive any votes.
+        Your task is to put together a winning strategy by identifying the fewest issues you must address to win 270 votes.
+
+        Input sample:
+        Your program should accept a file as its first argument. The file content is the following:
+
+        Social issues: 9
+
+        Healthcare: 33995797
+        Immigration: 2089699
+        Education: 37182280
+        Energy independence: 1344134
+        ...
+        Wealth inequality: 99237127
+        Increase military spending: 44066575
+
+        Mississippi
+        Votes: 6
+        Creating jobs: 1
+        Jobs: 0
+        ...
+        Increase military spending: 0
+        Education: 1
+        Energy independence: 0
+
+        Oklahoma
+        Votes: 7
+        Creating jobs: 1
+        Jobs: 2
+        ...
+        Increase military spending: 0
+        Education: 1
+        Energy independence: 2
+        ...
+        Maine
+        Votes: 4
+        Creating jobs: 0
+        Jobs: 0
+        ...
+        Increase military spending: 0
+        Education: 1
+        Energy independence: 3
+
+        The first line identifies the total number of potential issues, the second—information about costs for each program, and the third — information about each state, separated by spacing. Each state has a name, number of votes, and a list of issues that you can choose in each state. Each of these issues is valued based on the number of votes it can get in each state. Remember, you must get the majority of votes in each state to win.
+
+        Output sample:
+
+        Print out the list of issues you want to cover in you electoral program in alphabetical order.
+
+        For example:
+
+        Energy independence
+        Healthcare
+        Immigration
+        Increase military spending
+
+        Remember, that your task is to create a program with the fewest number of issues. If there are several variants of program with the fewest number of issues, then you need to choose the program with minimum costs.
+*/
+
+
 package runningForPresident;
 
 import java.io.FileNotFoundException;
@@ -9,32 +81,18 @@ import java.util.*;
  */
 public class Main {
     private int[][] voteTable;
-    private HashMap<String, Integer> issueToId;
-    private HashMap<Integer, String> idToIssue;
+    private String[] idToIssue;
     private HashMap<String, Integer> issueToCost;
     private int numOfIssues, numOfStates;
 
-    public Main(int[][] voteTable, HashMap<String, Integer> issueToCost, HashMap<String, Integer> issueToId, final HashMap<Integer, String> idToIssue, int numOfIssues, int numOfStates){
+    public Main(int[][] voteTable, HashMap<String, Integer> issueToCost, String[] idToIssue, int numOfIssues, int numOfStates){
         this.voteTable = voteTable;
-        this.issueToId = issueToId;
         this.idToIssue = idToIssue;
         this.issueToCost = issueToCost;
         this.numOfIssues = numOfIssues;
         this.numOfStates = numOfStates;
 
-        LinkedList<LinkedList<Integer>> combs = new LinkedList<LinkedList<Integer>>();
-        combs = genCombinations(2);
-
-        /*for(LinkedList<Integer> list : combs)
-            System.out.print(list.toString() + ",");
-
-        LinkedList<Integer> list = new LinkedList<Integer>();
-        list.add(3);
-        System.out.println();
-        System.out.println(getVotes( list, 2));
-        System.out.println(getVotes( list, 1));
-        */
-        System.out.println(idToIssue.toString());
+        //System.out.println(idToIssue.toString());
         LinkedList<Integer> minCostList = null;
         for(int i = 1; i <= numOfIssues; i++){
             int minCost = Integer.MAX_VALUE;
@@ -43,35 +101,28 @@ public class Main {
                 for(int j = 0; j < numOfStates; j++)
                     total += getVotes(list, j);
                 int listCost = issuesToCost(list);
-                System.out.println(list.toString() + " VOTES: "  + total + " COST: " + listCost);
+                //System.out.println(list.toString() + " VOTES: "  + total + " COST: " + listCost);
+                //find min
                 if(total >= 270 && listCost < minCost){
                     minCostList = list;
                     minCost = listCost;
                 }
             }
             if(minCostList != null) {
-                System.out.println("Minimum cost list: " + minCostList.toString() + " COST: " + minCost);
+                //System.out.println("Minimum cost list: " + minCostList.toString() + " COST: " + minCost);
                 break;
             }
         }
         TreeSet<String> sortedSet = new TreeSet<String>();
-        for(int id : minCostList) sortedSet.add(idToIssue.get(id));
+        for(int id : minCostList) sortedSet.add(idToIssue[id]);
         for(String s : sortedSet) System.out.println(s);
     }
 
     private int issuesToCost(LinkedList<Integer> issuesId){
         int total = 0;
         for(int id : issuesId)
-            total += issueToCost.get(idToIssue.get(id));
+            total += issueToCost.get(idToIssue[id]);
         return total;
-    }
-
-    public static int parseNum(String line){
-        return Integer.parseInt(line.split(":")[1].trim());
-    }
-
-    public static String parseIssue(String line){
-        return line.split(":")[0].trim();
     }
 
     public String stateToString(int stateId){
@@ -79,9 +130,17 @@ public class Main {
         toString += "STATE " + stateId + "\n";
         toString += "Number of votes: " + voteTable[0][stateId] + "\n";
         for(int i = 0; i < numOfIssues; i++)
-            toString += idToIssue.get(i) + ": " + voteTable[i + 1][stateId] + "\n";
-
+            toString += idToIssue[i] + ": " + voteTable[i + 1][stateId] + "\n";
         return toString;
+    }
+
+    private int getVotes(LinkedList<Integer> issues, int stateId){
+        int totalVotes = voteTable[0][stateId];
+        int votesForIssues = 0;
+        for(int issue : issues)
+            votesForIssues += voteTable[issue + 1][stateId];
+        // to receive all votes for the state, need to have majority of votes
+        return ( (Double.valueOf(votesForIssues) / Double.valueOf(totalVotes) ) > 0.50) ? totalVotes : 0;
     }
 
     private LinkedList<LinkedList<Integer>> genCombinations(int n){
@@ -92,14 +151,6 @@ public class Main {
         LinkedList<Integer> curr = new LinkedList<Integer>();
         genCombinations(arr, n, 0, acc, curr);
         return acc;
-    }
-
-    private int getVotes(LinkedList<Integer> issues, int stateId){
-        int totalVotes = voteTable[0][stateId];
-        int votesForIssues = 0;
-        for(int issue : issues)
-            votesForIssues += voteTable[issue + 1][stateId];
-        return ( (Double.valueOf(votesForIssues) / Double.valueOf(totalVotes) ) > 0.50) ? totalVotes : 0;
     }
 
     private void genCombinations(int[] arr, int n, int start, LinkedList<LinkedList<Integer>> acc, LinkedList<Integer> curr){
@@ -113,37 +164,46 @@ public class Main {
             genCombinations(arr, n, i + 1, acc, curr);
             curr.removeLast();
         }
-
-
+    }
+    public static int parseNum(String line){
+        return Integer.parseInt(line.split(":")[1].trim());
     }
 
+    public static String parseIssue(String line){
+        return line.split(":")[0].trim();
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
-        HashMap<String, Integer> issueToCost = new HashMap<String, Integer>();
-        HashMap<String, Integer> issueToId = new HashMap<String, Integer>();
-        HashMap<Integer, String> idToIssue = new HashMap<Integer, String>();
+        final int numOfStates, numOfIssues;
+        HashMap<String, Integer> issueToCost, issueToId;
+        int[][] voteTable;
+        String[] idToIssue;
+
         Scanner textScan = new Scanner(new FileReader("test-cases/runningForPresident.txt"));
 
-        int numOfStates = 51;
-        int numOfIssues = parseNum(textScan.nextLine());
+        numOfStates = 51;
+        numOfIssues = parseNum(textScan.nextLine());
+        issueToCost = new HashMap<String, Integer>();issueToId = new HashMap<String, Integer>();
+        idToIssue = new String[numOfIssues];
         // columns => states, rows => issue, last column => sums for each issue, first row => n. of votes for the state
-        int[][] voteTable = new int[numOfIssues + 1][numOfStates + 1];
+        voteTable = new int[numOfIssues + 1][numOfStates + 1];
 
         // skip empty line
         textScan.nextLine();
-        // parse issues
+        // parse issues and their costs
         for(int i = 0; i < numOfIssues; i++){
             String line = textScan.nextLine();
             String issue = parseIssue(line);
             issueToCost.put(issue, parseNum(line));
             issueToId.put(issue, i);
-            idToIssue.put(i, issue);
+            idToIssue[i] = issue;
         }
 
         for(int state = 0; state < numOfStates; state++){
             // skip empty line and name of state
             textScan.nextLine();
             textScan.nextLine();
+            // save number of total votes for this state
             int numOfVotes = parseNum(textScan.nextLine());
             voteTable[0][state] = numOfVotes;
             // parse states
@@ -157,15 +217,7 @@ public class Main {
                 voteTable[id+1][numOfStates] += votesForIssue;
             }
         }
-        System.out.println(Arrays.deepToString(voteTable));
 
-        for(int i = 0; i < numOfIssues; i ++)
-            System.out.println(voteTable[i + 1][numOfStates]);
-
-        Main test = new Main(voteTable, issueToCost, issueToId, idToIssue, numOfIssues, numOfStates);
-        //System.out.println(test.stateToString(0));
-        //System.out.println(test.stateToString(48));
-        //System.out.println(test.stateToString(50));
-
+        Main test = new Main(voteTable, issueToCost, idToIssue, numOfIssues, numOfStates);
     }
 }
