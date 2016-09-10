e607d087a399bec287e810c0643ed0208e55f562
RobertH <robberth.cz@gmail.com>
2016-09-10 16:45:19 +0200

    bus network works 100%

--- src/busNetwork/Main.java
+++ src/busNetwork/Main.java
@@ -1,4 +1,35 @@
-package BusTour;
+/**
+ A BUS NETWORK
+ CHALLENGE DESCRIPTION:
+ You have just moved to a new city where the main transport facility is a bus. You have noticed that for each bus route, it takes a bus 7 minutes to ride between the adjacent stops, and it takes 12 minutes for you to change a bus. To use your time more efficiently, write a program that determines how to get from point A to point B in the least amount of time using a following list of bus routes.
+ The bus route R1 a list of bus stops ID represented in the same order as they appear on the route. You can change a bus only at the stop with the same ID.
+
+ For example:
+ R1=[1,2,3,4]; R2=[5,6,4]; R3=[9,6,7]; R4=[12,1,2,3,11,16,15,14,10,7]
+ To get from point 1 to point 7, you can start from the bus route R1, and ride to the stop with ID=4, change the route R1 to the route R2 to reach the stop with ID=6, then change R2 to R3 to arrive at the stop with ID=7, or you can simply take the bus route R4, occupy a seat, relax, and ride straight to a stop with ID=7. Your task is to determine the fastest possible route.
+
+ INPUT SAMPLE:
+ Your program should accept a path to a filename as its first argument. Each line in the file is one test case. Each test case contains the points A and B in brackets and a list of routes separated by a semicolon.
+ For example:
+ (2,4); R1=[1,2,3,11,12,4]; R2=[5,6,4]; R3=[1,6,7]; R4=[5,6,4]; R5=[8,6 ,3]
+ (1,7); R1=[1,2,3,4]; R2=[5,6,4]; R3=[9,6,7]; R4=[12,1,2,3,11,16,15,14,10 ,13,7]
+ (3,299); R1=[1,2,3,4]; R2=[6,7,19,12,4]; R3=[11,14,16,6]; R4=[24,299,42 ,6]
+ (3,4); R1=[1,2,3]; R2=[6,7,19,12,4]; R3=[11,14,16,6]
+
+ OUTPUT SAMPLE:
+ For each test case print out the minimum time in minutes needed to get from point A to point B using the given bus routes or print out "None" if there is no connection between these two points.
+ For example:
+ 28
+ 59
+ 73
+ None
+
+ Constraints:
+ The number of routes is in a range from 3 to 150.
+ The IDs are in a range from 0 to 300.
+ The length of a route is in a range from 10 to 35 stops.
+ */
+package busNetwork;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -37,19 +68,17 @@ public class Main {
 		}
 	}
 
-	public HashMap<Vertex, Integer> dijkstraSP(Vertex startV){
+	public int dijkstraSP(int start, int destination){
 		HashMap<Vertex, Integer> distTo = new HashMap<Vertex, Integer>();
 		TreeSet<Vertex> Q = new TreeSet<Vertex>();
 
-		distTo.put(startV, 0);
-		Q.add(startV);
+        for(Vertex startV : stopToVertex.get(start)){
+            distTo.put(startV, 0);
+            Q.add(startV);
+        }
 		while(!Q.isEmpty()){
-			//System.out.println("Q size: " + Q.size());
 			Vertex min = Q.pollFirst();
-			//System.out.println(min);
-			//System.out.println("Number of adj. edges: " + adj.get(min).size());
 			for(Edge e : adj.get(min)){
-				//System.out.println("Edge to: " + e.to);
 				if(!distTo.containsKey(e.to) || distTo.get(e.to) > (e.cost + distTo.get(e.from))){
 					distTo.put( e.to, e.cost + distTo.get(e.from));
 					Q.remove(e.to);
@@ -58,7 +87,13 @@ public class Main {
 				}
 			}
 		}
-		return distTo;
+        // find min
+        int min = Integer.MAX_VALUE;
+        for(Vertex endV : stopToVertex.get(destination)){
+            if(distTo.containsKey(endV))
+                min = Math.min(min, distTo.get(endV));
+        }
+		return min;
 	}
 
 	class Vertex implements Comparable<Vertex>{
@@ -137,53 +172,35 @@ public class Main {
 	}
 
 	public static void main(String args[]) throws FileNotFoundException {
+		Scanner textScan = new Scanner(new FileReader("src/busNetwork/input_large.txt"));
 
-		Scanner scanBusRoutes = new Scanner(new FileReader("src/BusTour/input.txt"));
-
-		while (scanBusRoutes.hasNextLine()) {
+		while (textScan.hasNextLine()) {
 			Main test = new Main();
+			String line = textScan.nextLine();
+            // remove all bullshit
+            line = line.replaceAll("\\(|\\)|\\]| R|=", "");
+			String routes[] = line.split(";");
 
-			String line = scanBusRoutes.nextLine();
+			int start = Integer.parseInt(routes[0].split(",")[0]);
+			int destination = Integer.parseInt(routes[0].split(",")[1]);
 
-			String words[] = line.split("\\s+");
+			for (int i = 1; i < routes.length; i++) {
+				String route = routes[i].substring(routes[i].indexOf("[") + 1);
+				String[] busStops = route.split(",");
 
-			int startId = words[0].indexOf(",");
-			int destinationId = words[0].indexOf(")");
-			int start = Integer.parseInt(words[0].substring(1, startId));
-			int destination = Integer.parseInt(words[0].substring(startId + 1, destinationId));
-
-			for (int i = 1; i < words.length; i++) {
-				int routeStartId = words[i].indexOf("[");
-				int routeEndId = words[i].indexOf("]");
-				String busStops = words[i].substring(routeStartId + 1, routeEndId);
-				String[] busStop = busStops.split(",");
-
-				for (int j = 0; j < busStop.length - 1; j++) {
-
-					int busStop1 = Integer.parseInt(busStop[j]);
-					int busStop2 = Integer.parseInt(busStop[j + 1]);
-					test.addStopEdge(i, busStop1, busStop2);
-					test.addStopEdge(i, busStop2, busStop1);
+				for (int j = 0; j < busStops.length - 1; j++) {
+					int bs1 = Integer.parseInt(busStops[j]);
+					int bs2 = Integer.parseInt(busStops[j + 1]);
+					test.addStopEdge(i, bs1, bs2);
+					test.addStopEdge(i, bs2, bs1);
 				}
 			}
+            test.addBusChangeEdges();
 
-			HashSet<Vertex> startVs = test.stopToVertex.get(start);
-			HashSet<Vertex> endVs = test.stopToVertex.get(destination);
-			int min = Integer.MAX_VALUE;
-			test.addBusChangeEdges();
-
-			for(Vertex v : startVs){
-				HashMap<Vertex, Integer> distTo = test.dijkstraSP(v);
-				for(Vertex endV : distTo.keySet()){
-					if(endVs.contains(endV) && distTo.get(endV) < min)
-						min = distTo.get(endV);
-				}
-			}
+			int min = test.dijkstraSP(start, destination);
 			if(min == Integer.MAX_VALUE) System.out.println("None");
 			else System.out.println(min);
-
-			// results should be 28 - 59 - 73 - None - 33
-
+			// results should be 28 - 59 - 73 - None - 33 for small input
 		}
 	}
 
