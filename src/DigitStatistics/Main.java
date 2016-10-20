4bfbccda5b7a26b5a9f6690841888e185b0c6b97
Robert Havelka <robberth.cz@gmail.com>
2016-10-20 14:19:27 +0200

    digit statistics refactored

--- src/DigitStatistics/Main.java
+++ src/DigitStatistics/Main.java
@@ -1,3 +1,22 @@
+/**
+ Digit statistics
+ Challenge Description:
+ Given the numbers "a" and "n" find out how many times each digit from zero to nine is the last digit of the number in a sequence [ a, a2, a3, ... an-1, an ]
+
+ Input sample:
+ Your program should accept as its first argument a path to a filename. Each line of input contains two space separated integers "a" and "n" E.g:
+ 2 5
+
+ Output sample
+ For each line of input print out how many times the digits zero, one, two ... nine occur as the last digit of numbers in the sequence E.g:
+ 0: 0, 1: 0, 2: 2, 3: 0, 4: 1, 5: 0, 6: 1, 7: 0, 8: 1, 9: 0
+
+ In this example, the sequence consists of numbers 2, 4, 8, 16 and 32. Among the last digits, the digit two occurs twice, and each of the digits four, six and eight occurs once.
+
+ Constraints:
+ 1 ≤ n ≤ 1 000 000 000 000,
+ 2 ≤ a ≤ 9
+ */
 package DigitStatistics;
 
 import java.io.FileNotFoundException;
@@ -6,10 +25,6 @@ import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 /**
- * We are given numbers "a" and "n". We count how many times digits between 0
- * and 9 are last digits of numbers in a sequence (a to power of 1,..., a to
- * power of n-1, a to power of n)
- * 
  * @author Robb
  *
  */
@@ -22,17 +37,12 @@ public class Main {
 		lastDigs = new LinkedHashSet<Integer>();
 		// use only the last digit
 		a = a % 10;
-
-		int aCopy = a % 10;
+		int aCopy = a;
 
 		while (!lastDigs.contains(aCopy)) {
-			// System.out.println(aCopy);
-
 			lastDigs.add(aCopy);
 			aCopy = aCopy * a % 10;
-
 		}
-
 		// contains the number of times given number is last digit
 		long[] lastDigits = new long[10];
 
@@ -42,20 +52,14 @@ public class Main {
 		long leftover = n - nOfRepeats * lastDigs.size();
 
 		for (int digit : lastDigs) {
-			// signifies whether the last digit is in the last incomplete cycle
-			int one;
-
-			// whether last incomplete cycle contains this processed last digit
+            lastDigits[digit] = nOfRepeats;
+		    // whether last incomplete cycle contains this processed last digit
 			if (leftover > 0) {
-				one = 1;
+				lastDigits[digit] += 1;
 				// decrement the length of last incomplete cycle
 				// every cycle is ordered the same way, like 1,2,3 1,2,3 1,2
 				leftover--;
-			} else
-				one = 0;
-
-			lastDigits[digit] = nOfRepeats + one;;
-
+			}
 		}
 
 		// solution is submitted in the form that 0: 2, 1: 5 ... 9: 3
@@ -63,19 +67,15 @@ public class Main {
 
 		for (int i = 0; i < 10; i++) {
 			toReturn.append(i + ": " + lastDigits[i]);
-			if (i != 9)
-				toReturn.append(", ");
-
+			if (i != 9) toReturn.append(", ");
 		}
-
 		// submit the solutions
 		System.out.println(toReturn);
-
 	}
 
 	public static void main(String args[]) throws FileNotFoundException {
 		Scanner textScan = new Scanner(new FileReader(
-				"test-cases/digitStatistics.txt"));
+				"src/DigitStatistics/input.txt"));
 
 		while (textScan.hasNextLine()) {
 			String words[] = textScan.nextLine().split(" ");
@@ -85,7 +85,5 @@ public class Main {
 
 			Main test = new Main(a, n);
 		}
-
 	}
-
 }
