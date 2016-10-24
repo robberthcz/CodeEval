7ba96b383cfa0f8bb9d2f65255eac7a3d98d16fa
Robert Havelka <robberth.cz@gmail.com>
2016-10-24 15:02:46 +0200

    robot movements refactored

--- src/RobotMovements/Main.java
+++ src/RobotMovements/Main.java
@@ -1,25 +1,18 @@
+/**
+ Robot Movements
+ Challenge Description:
+ A robot is located in the upper-left corner of a 4×4 grid. The robot can move either up, down, left, or right, but cannot go to the same location twice. The robot is trying to reach the lower-right corner of the grid. Your task is to find out the number of unique ways to reach the destination.
+
+ Input sample:
+ There is no input for this program.
+
+ Output sample:
+ Print out the number of unique ways for the robot to reach its destination. The number should be printed out as an integer ≥0.
+ */
 package RobotMovements;
 
 import java.util.LinkedList;
-/**
- * 
- * A robot is located in the upper-left corner of a 4×4 grid. The robot can move
- * either up, down, left, or right, but cannot go to the same location twice.
- * The robot is trying to reach the lower-right corner of the grid. Your task is
- * to find out the number of unique ways to reach the destination.
- * 
- * Input sample:
- * 
- * There is no input for this program.
- * 
- * Output sample:
- * 
- * Print out the number of unique ways for the robot to reach its destination.
- * The number should be printed out as an integer ≥0.
- * 
- * @author Robb
- *
- */
+
 public class Main {
 	private int boardRows, boardCols, count;
 	// count - counts the number of unique ways
@@ -34,11 +27,9 @@ public class Main {
 		// start from upper left corner whose index is 0 in 1-dimensional
 		// coordinate system
 		dfs(0, marked);
-
 	}
 
 	private void dfs(int v, boolean[] marked) {
-	
 		// v is the 1-dimensional index of lower right corner of 4x4 grid
 		// here we reached the end
 		if (v == 15) {
@@ -52,10 +43,8 @@ public class Main {
 
 		// explore all adjacent cells
 		for (int w : adj(v)) {
-			if (!marked[w]) {
+			if (!marked[w])
 				dfs(w, marked);
-			}
-
 		}
 		// backtrack
 		marked[v] = false;
@@ -77,15 +66,10 @@ public class Main {
 		int row = from1Dto2D(toExplore)[0];
 		int column = from1Dto2D(toExplore)[1];
 
-		if (row > 0)
-			adj.add(from2Dto1D(row - 1, column));
-		if (row < boardRows - 1)
-			adj.add(from2Dto1D(row + 1, column));
-		if (column > 0)
-			adj.add(from2Dto1D(row, column - 1));
-		if (column < boardCols - 1)
-			adj.add(from2Dto1D(row, column + 1));
-
+		if (row > 0)                adj.add(from2Dto1D(row - 1, column));
+		if (row < boardRows - 1)	adj.add(from2Dto1D(row + 1, column));
+		if (column > 0)			    adj.add(from2Dto1D(row, column - 1));
+		if (column < boardCols - 1) adj.add(from2Dto1D(row, column + 1));
 		return adj;
 	}
 
@@ -94,23 +78,18 @@ public class Main {
 	private int[] from1Dto2D(int index) {
 		int row = index / boardCols;
 		int column = index % boardCols;
-		if (row == 0)
-			column = index;
+		if (row == 0) column = index;
 
 		int[] toReturn = {row, column};
 		return toReturn;
 	}
 
 	private int from2Dto1D(int row, int column) {
-
-		if (row == 0)
-			return column;
-		else
-			return column + row * boardCols;
+		if (row == 0)	return column;
+		else			return column + row * boardCols;
 	}
 
 	public static void main(String args[]) {
-
 		Main test = new Main();
 		System.out.println(test.getUniqueWays());
 	}
