d0d4cc9eacfbd82c32da6b972bb9189222d9e6a6
Robert Havelka <robberth.cz@gmail.com>
2016-10-25 13:28:23 +0200

    string list refactored

--- src/StringList/Main.java
+++ src/StringList/Main.java
@@ -1,3 +1,21 @@
+/**
+ STRING LIST
+ CHALLENGE DESCRIPTION:
+ Credits: Challenge contributed by Max Demian.
+ You are given a number N and a string S. Print all of the possible ways to write a string of length N from the characters in string S, comma delimited in alphabetical order.
+
+ INPUT SAMPLE:
+ The first argument will be the path to the input filename containing the test data. Each line in this file is a separate test case. Each line is in the format: N,S i.e. a positive integer, followed by a string (comma separated). E.g.
+ 1,aa
+ 2,ab
+ 3,pop
+
+ OUTPUT SAMPLE:
+ Print all of the possible ways to write a string of length N from the characters in string S comma delimited in alphabetical order, with no duplicates. E.g.
+ a
+ aa,ab,ba,bb
+ ooo,oop,opo,opp,poo,pop,ppo,ppp
+ */
 package StringList;
 
 import java.io.FileNotFoundException;
@@ -5,31 +23,6 @@ import java.io.FileReader;
 import java.util.Scanner;
 import java.util.TreeSet;
 
-/**
- * Challenge Description:
- * 
- * Credits: Challenge contributed by Max Demian.
- * 
- * You are given a number N and a string S. Print all of the possible ways to
- * write a string of length N from the characters in string S, comma delimited
- * in alphabetical order.
- * 
- * Input sample:
- * 
- * The first argument will be the path to the input filename containing the test
- * data. Each line in this file is a separate test case. Each line is in the
- * format: N,S i.e. a positive integer, followed by a string (comma separated).
- * E.g. 1,aa 2,ab 3,pop
- * 
- * Output sample:
- * 
- * Print all of the possible ways to write a string of length N from the
- * characters in string S comma delimited in alphabetical order, with no
- * duplicates. E.g. a aa,ab,ba,bb ooo,oop,opo,opp,poo,pop,ppo,ppp
- * 
- * @author Robb
- *
- */
 public class Main {
 	StringBuilder lastWord;
 	int len;
@@ -42,15 +35,12 @@ public class Main {
 		for (int i = 0; i < word.length(); i++)
 			set.add(word.charAt(i));
 
-		char[] keypad = new char[set.size()];
-
-		int id = 0;
-		for (char c : set) {
-			keypad[id++] = c;
-		}
+        char[] keypad = new char[set.size()];
+      	int id = 0;
+		for (char c : set) keypad[id++] = c;
 
 		// just to know when stop printing ","
-		lastWord = new StringBuilder("");
+		this.lastWord = new StringBuilder("");
 		for (int i = 0; i < len; i++)
 			lastWord.append(keypad[keypad.length - 1]);
 
@@ -59,47 +49,34 @@ public class Main {
 			dfs(0, i, keypad, permutation);
 		}
 		System.out.println();
-
 	}
 
-	private void dfs(int row, int column, char[] keypad,
-			StringBuilder permutation) {
-
+	private void dfs(int row, int column, char[] keypad, StringBuilder permutation) {
 		permutation.append(keypad[column]);
 
 		if (row == len - 1) {
-
 			System.out.print(permutation);
 
 			if (!permutation.toString().equals(lastWord.toString()))
 				System.out.print(",");
-
-			permutation = permutation.deleteCharAt(permutation.length() - 1);
+			permutation.deleteCharAt(permutation.length() - 1);
 			return;
 		}
 
-		for (int j = 0; j < keypad.length; j++) {
+		for (int j = 0; j < keypad.length; j++)
 			dfs(row + 1, j, keypad, permutation);
-
-		}
-		permutation = permutation.deleteCharAt(permutation.length() - 1);
-
+        // modify back the intermediate string for other recursive calls
+		permutation.deleteCharAt(permutation.length() - 1);
 	}
 
 	public static void main(String args[]) throws FileNotFoundException {
-
-		Scanner textScan = new Scanner(new FileReader("stringList.txt"));
+		Scanner textScan = new Scanner(new FileReader("src/StringList/input.txt"));
 
 		while (textScan.hasNextLine()) {
-
 			String line = textScan.nextLine();
-
 			String words[] = line.split(",");
-
 			int len = Integer.parseInt(words[0]);
-
 			Main test = new Main(words[1], len);
 		}
-
 	}
 }
