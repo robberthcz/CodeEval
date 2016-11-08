3508408f3dfd46e33c9c89ca518e87456a0fcfda
Robert Havelka <robberth.cz@gmail.com>
2016-11-08 15:21:28 +0100

    play with DNA still not 100%

--- src/playWithDNA/Main.java
+++ src/playWithDNA/Main.java
@@ -36,28 +36,56 @@ package playWithDNA;
  */
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     private String pattern, text;
     private int k;
+    private HashMap<String, Integer> matchToK;
 
     public Main(String pattern, int k, String text){
         this.pattern = pattern;
         this.text = text;
         this.k = k;
-        String S = "GATTACA";
-        String T = "GCATGCU";
+        //this.k = 2;
+       // this.pattern = "CACD";
+        //this.text = "BCBACBBB";
+        this.matchToK = new HashMap<>();
 
-        int[][] score = NW( S, T);
+        int[][] score = DP( this.pattern, this.text);
+        LinkedList<String> results = new LinkedList<>();
+
         printArray(score);
-        System.out.println();
-        score = NW(S, T.substring(1) + 'C');
-        printArray(score);
-        //String match = reconstruct(score, S, T, score.length - 1, score[0].length - 1,"");
-        //System.out.println(match);
-        //System.out.println(getMismatches(S, match));
+
+        for(int i = 1; i < score[0].length; i++){
+            if(score[score.length - 1][i] > this.k)
+                continue;
+            Set<String> set = new HashSet<>();
+            reconstruct(score, score.length - 1, i, 0, "", set);
+            if(!set.isEmpty()){
+                for(String S : set){
+                    matchToK.put(S, score[score.length - 1][i]);
+                }
+                results.addAll(set);
+            }
+        }
+
+        Collections.sort(results, new MISMATCH_ORDER());
+        String res = results.toString();
+        res = res.substring(1, res.length() - 1).replaceAll(",", "");
+        if(res.equals(""))
+            res = "No match";
+        System.out.println(res);
+    }
+
+    public final class MISMATCH_ORDER implements Comparator<String>{
+        public int compare(String S, String T){
+            int kS = matchToK.get(S);
+            int kT = matchToK.get(T);
+
+            if(kS != kT) return Integer.compare(kS, kT);
+            else return S.compareTo(T);
+        }
     }
 
     private void printArray(int[][] a){
@@ -67,57 +95,45 @@ public class Main {
         }
     }
 
-    private int[][] NW(String S, String T){
-        int[][] score = new int[S.length() + 1][T.length() + 1];
+    private int[][] DP(String pattern, String text){
+        int[][] score = new int[pattern.length() + 1][text.length() + 1];
 
-        for(int i = 1; i < score.length; i++){
-            score[i][0] = i * (-1);
+        for(int i = 0; i < score.length; i++){
+            score[i][0] = i;
         }
 
-        for(int i = 1; i < score[0].length; i++){
-            score[0][i] = i * (-1);
-        }
+        for(int j = 1; j < score[0].length; j++){
+            for(int i = 1; i < score.length; i++){
+                int match = pattern.charAt(i-1) == text.charAt(j-1) ? 0 : 1;
+                score[i][j] = Math.min(score[i-1][j-1] + match, Math.min(score[i-1][j] + 1, score[i][j-1] + 1));
 
-        for(int i = 1 ; i < score.length; i++){
-            for(int j = 1; j < score[0].length; j++){
-                int match = (S.charAt(i - 1) == T.charAt(j - 1)) ? 1 : -1;
-                score[i][j] = Math.max(score[i-1][j-1] + match, Math.max(score[i-1][j] - 1, score[i][j-1] - 1));
-                //System.out.println("i: " + i + ", j: " + j + ", score: " + score[i][j]);
             }
         }
-
         return score;
     }
 
-    private String reconstruct(int[][] score, String S, String T, int row, int col, String tmp){
-        if(row == 0 && col == 0) return tmp;
-        int match = (S.charAt(row - 1) == T.charAt(col - 1)) ? 1 : -1;
-
-        if      (score[row - 1][col - 1] + match == score[row][col]) return reconstruct(score, S, T, row-1, col-1, T.charAt(col - 1) + tmp);
-        else if (score[row - 1][col] - 1 == score[row][col])         return reconstruct(score, S, T, row-1, col, '-' + tmp);
-        else                                                         return reconstruct(score, S, T, row, col-1, '+' + tmp);
-    }
-
-    private int getMismatches(String S, String temp){
-        int mismatches = 0;
-        int j = 0;
-        for(int i = 0; i < temp.length(); i++){
-            if(S.charAt(j) == temp.charAt(i))
-                mismatches++;
-
-            if(temp.charAt(i) != '+')
-                j++;
+    private void reconstruct(int[][] a, int row, int col, int len, String tmp, Set<String> set){
+        //System.out.println("Row: " + row + ", col: " + col + ", a: " + a[row][col]);
+        if(row == 0){
+            //System.out.println(len + " " + pattern.length());
+            if(len == pattern.length())
+                set.add(tmp);
+            return;
         }
-        return mismatches;
-    }
+        else if(row <= 0 || col <= 0)
+            return;
 
+        int match = (pattern.charAt(row - 1) == text.charAt(col - 1)) ? 0 : 1;
+
+        if (a[row - 1][col - 1] + match == a[row][col]) reconstruct(a, row - 1, col - 1, len+1, text.charAt(col-1) + tmp, set);
+        if(a[row - 1][col] + 1 == a[row][col])          reconstruct(a, row-1, col, len, tmp, set);
+        if(a[row][col-1] + 1 == a[row][col])            reconstruct(a, row, col-1, len+1, text.charAt(col-1) + tmp, set);
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner textScan = new Scanner(new FileReader("src/playWithDNA/input_large.txt"));
 
-        Main test = new Main("", 0,"");
-
-        /*while(textScan.hasNextLine()){
+        while(textScan.hasNextLine()){
             String line[] = textScan.nextLine().trim().split(" ");
 
             String pattern = line[0];
@@ -125,12 +141,6 @@ public class Main {
             String text = line[2];
 
             Main test = new Main(pattern, k, text);
-
-            String res = test.search().toString();
-            res = res.substring(1, res.length() - 1).replaceAll(",", "");
-            if(res.equals(""))
-                res = "No match";
-            System.out.println(res);
-        }*/
+        }
     }
 }
