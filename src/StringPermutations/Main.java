3bbc13eb50df0aedb44d38f5d07e93c5c5b5b92b
RobertH <robberth.cz@gmail.com>
2016-11-18 20:26:02 +0100

    string permuations refactored

--- src/StringPermutations/Main.java
+++ src/StringPermutations/Main.java
@@ -1,3 +1,22 @@
+/**
+ STRING PERMUTATIONS
+ CHALLENGE DESCRIPTION:
+ Write a program which prints all the permutations of a string in alphabetical order. We consider that digits < upper case letters < lower case letters. The sorting should be performed in ascending order.
+
+ INPUT SAMPLE:
+ Your program should accept a file as its first argument. The file contains input strings, one per line.
+ For example:
+ hat
+ abc
+ Zu6
+
+ OUTPUT SAMPLE:
+ Print to stdout the permutations of the string separated by comma, in alphabetical order.
+ For example:
+ aht,ath,hat,hta,tah,tha
+ abc,acb,bac,bca,cab,cba
+ 6Zu,6uZ,Z6u,Zu6,u6Z,uZ6
+ */
 package StringPermutations;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,49 +26,9 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-/**
- * Challenge Description:
- * 
- * Write a program which prints all the permutations of a string in alphabetical
- * order. We consider that digits < upper case letters < lower case letters. The
- * sorting should be performed in ascending order.
- * 
- * Input sample:
- * 
- * Your program should accept a file as its first argument. The file contains
- * input strings, one per line.
- * 
- * For example: 
- * hat
- * 
- * abc
- * 
- * Zu6
- * 
- * 
- * Output sample:
- * 
- * Print to stdout the permutations of the string separated by comma, in
- * alphabetical order.
- * 
- * For example:
- * 
- * 
- * aht,ath,hat,hta,tah,tha
- * 
- * abc,acb,bac,bca,cab,cba
- * 
- * 6Zu,6uZ,Z6u,Zu6,u6Z,uZ6
- * 
- * 
- * 
- * @author Robb
- *
- */
 public class Main {
 
 	public static Iterable<String> genPermutation(String s, int size) {
-
 		int N = s.length();
 		Queue<String> toReturn = new LinkedList<String>();
 		boolean[] marked = new boolean[N];
@@ -61,14 +40,11 @@ public class Main {
 		genPermutation(-1, size, permutation, marked, sChars, toReturn);
 
 		return toReturn;
-
 	}
 
 	private static void genPermutation(int level, int size, char[] permutation,
 			boolean[] marked, char[] str, Queue<String> toReturn) {
-
 		if (level >= size - 1) {
-
 			toReturn.add(String.valueOf(permutation));
 			return;
 		}
@@ -85,7 +61,6 @@ public class Main {
 				level--;
 			}
 		}
-
 	}
 
 	public static void main(String args[]) throws FileNotFoundException {
@@ -93,22 +68,15 @@ public class Main {
 
 		while (strings.hasNextLine()) {
 			String word = strings.nextLine();
-
 			Iterable<String> permutations = Main.genPermutation(word, 3);
-
 			Iterator<String> iter = permutations.iterator();
 
 			while (iter.hasNext()) {
 				System.out.print(iter.next());
 				if (iter.hasNext())
 					System.out.print(",");
-
 			}
-
 			System.out.println();
-
 		}
-
 	}
-
 }
